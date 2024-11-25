37c5c3d8eb27b9fac042da447ad3e834297fc553
Tugay <tasimtugay@gmail.com>
2024-11-26 00:51:20 +0300

    Login Page v0.1

---
+++ src/pages/login_page/login_page.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+import "./login_page.css"
+import { useNavigate } from 'react-router-dom';
+import { Button, Checkbox, Form, Input, message } from 'antd';
+
+import formImage from '../../assets/login_image.png'
+
+const loginDetails = [{
+    email : "someone@example.com",
+    password : "admin135"
+}];
+
+
+
+export default function LoginPage () {
+    const [messageApi, contextHolder] = message.useMessage();
+    
+    const navigate = useNavigate();
+
+    //Correct information entered
+    const onFinish = (values) => {
+        console.log('Success:', values);
+        
+        //Login deatils check
+        if (values.user.email === loginDetails[0].email && values.password === loginDetails[0].password){
+            console.log("Login Successful!")
+            navigate('/homepage');
+    
+            //......
+        }
+        else{
+            error();
+            console.log("Check your information again!");
+        }
+    
+      };
+
+      const error = () => {
+        messageApi.open({
+            type: 'error',
+            content: 'Error!',
+        });
+    };
+
+
+      // False information entered
+      const onFinishFailed = (errorInfo) => {
+        error();
+        console.log('Failed:', errorInfo);
+      };
+
+    return (
+        <div className="login-main">
+            {contextHolder}
+            <div className="login-container">
+                <div className="login-container-image">
+                    <img src={formImage} alt="" width={500} height={350}/>
+                </div>
+                
+                <div className="login-container-form">
+                    <h2>Sign In</h2>
+                    <Form
+                        name="basic"
+                        wrapperCol={{span: 24}}
+                        initialValues={{remember: true}}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off">
+
+                        <Form.Item
+                            name={['user', 'email']}
+                            rules={[
+                                {
+                                required: true,
+                                type: 'email',
+                                message: 'Please input your e-mail!'
+                                },
+                            ]}
+                            >
+                        
+                            <Input placeholder="E-mail"/>
+                        </Form.Item>
+
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}>
+                            <Input.Password placeholder="Password"/>
+                        </Form.Item>
+
+                        <Form.Item
+                            wrapperCol={{
+                                offset: 8,
+                                span: 16,
+                            }}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
+            {/* <span id="form-container">
+                <Form
+                    name="basic"
+                    wrapperCol={{span: 24}}
+
+                    initialValues={{remember: true}}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off">
+
+                    <Form.Item
+                        name={['user', 'email']}
+                        rules={[
+                            {
+                            required: true,
+                            type: 'email',
+                            message: 'Please input your e-mail!'
+                            },
+                        ]}
+                        >
+                    
+                    <Input placeholder="E-mail"/>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                            required: true,
+                            message: 'Please input your password!',
+                            },
+                        ]}>
+                    <Input.Password placeholder="Password"/>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 0,
+                            span: 16,
+                        }}>
+                    
+                    <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                    </Form.Item>
+                </Form>
+            </span> */}
+        </div>
+    );
+}
